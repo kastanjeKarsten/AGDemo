f05f1e96853339acf460235a1989552f9d29238c
Emil Stounberg <emil@aeroguest.com>
2019-03-19 08:30:55 +0100

    Initial Commit

---
+++ AGDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AGDemo
+//
+//  Created by Emil Stounberg on 19/03/2019.
+//  Copyright © 2019 AeroGuest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AGDemoTests/AGDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  AGDemoTests.swift
+//  AGDemoTests
+//
+//  Created by Emil Stounberg on 19/03/2019.
+//  Copyright © 2019 AeroGuest. All rights reserved.
+//
+
+import XCTest
+@testable import AGDemo
+
+class AGDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
